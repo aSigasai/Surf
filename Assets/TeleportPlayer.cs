b752d678000da2cc48aad33dda0f89d9cadc727f
Aaron Weiss <asigasai@gmail.com>
2017-06-03 22:15:47 -0400

    Closer and closer to source movement..

---
+++ Assets/TeleportPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayer : MonoBehaviour {
+
+    GameObject from, to;
+	// Use this for initialization
+	void Start () {
+        from = transform.Find("From").gameObject;
+        to = transform.Find("To").gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Collision");
+        GameObject.Find("RigidBodyFPSController").transform.position = to.transform.position;
+    }
+}
